05611e14a6b9fdf42f5f7699260bbe3f601291ed
Maia-Aedo <aedomaia10@gmail.com>
2024-09-07 12:24:34 -0300

    cambios en menu

---
+++ public/js/main.js
@@ -0,0 +1,6 @@
+function toggleNav() {
+    var nav = document.getElementById("nav");
+    var containerIconos = document.getElementById("container-iconos");
+    nav.classList.toggle("open");
+    containerIconos.classList.toggle("open")
+}
