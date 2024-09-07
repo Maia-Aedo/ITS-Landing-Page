c435f9a3096aef2868c5239b2c09bcf88367a3a2
Maia-Aedo <aedomaia10@gmail.com>
2024-09-07 13:52:30 -0300

    cambios en menu

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,20 @@
+// funcion para abrir el mnu
 function toggleNav() {
     var nav = document.getElementById("nav");
-    var containerIconos = document.getElementById("container-iconos");
+    var iconMenu = document.getElementById("bars")
+
+    /* al nav y container de íconos le añade la clase open del metodo toggle 
+    toggle agrega o elimina una clase al elto */
     nav.classList.toggle("open");
-    containerIconos.classList.toggle("open")
+
+     // alternar el menu de barras por equis
+     if (iconMenu.classList.contains("fa-bars")) {
+        // si el menu contiene el icono de barras, lo elimina y añade el de X
+        iconMenu.classList.remove("fa-bars");
+        iconMenu.classList.add("fa-times");
+    } else {
+        // si el menu es X lo cambia por las barras
+        iconMenu.classList.remove("fa-times");
+        iconMenu.classList.add("fa-bars");
+    }
 }
